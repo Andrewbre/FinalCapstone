8f81bb40d7ef34c7ec24be2d1bb7ae440110cc56
Andrew Brennan <andrewbrennan216@gmail.com>
2022-12-05 15:51:37 -0500

    controller new

--- java/src/main/java/com/techelevator/controller/EventController.java
+++ java/src/main/java/com/techelevator/controller/EventController.java
@@ -2,9 +2,11 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.EventDao;
 import com.techelevator.dao.UserDao;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class EventController {
 
 private EventDao eventDao;
