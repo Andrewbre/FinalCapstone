291e8df2cc7b6d198c3cda800a4bd67f6c2bffe0
Brian Brust <bbrust93@gmail.com>
2022-12-09 11:03:04 -0500

    backtofinal_capstone

--- java/src/test/java/com/techelevator/dao/JdbcEventDaoTests.java
+++ java/src/test/java/com/techelevator/dao/JdbcEventDaoTests.java
@@ -24,7 +24,7 @@ public class JdbcEventDaoTests extends BaseDaoTests {
     @Before
     public void setup() {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        
+
         sut = new JdbcEventDao(jdbcTemplate);
 
     }
