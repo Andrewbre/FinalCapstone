d11893d948853292256a36c36c9ff72c97c83ca1
Brian Brust <bbrust93@gmail.com>
2022-12-11 17:07:50 -0500

    made the event form!

--- vue/src/views/EventRegister.vue
+++ vue/src/views/EventRegister.vue
@@ -1,10 +1,21 @@
 <template>
+<div class = "eventRegistration">
     <h1>Event Registration</h1>
+    <event-registration-form> </event-registration-form>
+</div>    
 </template>
 
 <script>
 //import authService from "../services/AuthService";
+import EventRegistrationForm from "../components/EventRegistrationForm.vue"
+    export default {
+        name: "event-register",
+        components: {
+            EventRegistrationForm,
 
+        }
+
+    }
 </script>
 
 <style>
