5f87b614fb599b8cb26ce804d6dd55ab1dc3a4e7
Andrew Brennan <andrewbrennan216@gmail.com>
2022-12-14 11:53:17 -0500

    A

--- java/src/main/java/com/techelevator/model/spotify/Artist.java
+++ java/src/main/java/com/techelevator/model/spotify/Artist.java
@@ -49,14 +49,6 @@ public class Artist{
         this.uri = uri;
     }
 
-    public Artist(ExternalUrls external_urls, String href, String id, String name, String type, String uri) {
-        this.external_urls = external_urls;
-        this.href = href;
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.uri = uri;
-    }
 
     public ExternalUrls external_urls;
     public String href;
--- java/src/main/java/com/techelevator/model/spotify/Followers.java
+++ java/src/main/java/com/techelevator/model/spotify/Followers.java
@@ -1,10 +1,6 @@
 package com.techelevator.model.spotify;
 
 public class Followers{
-    public Followers(Object href, int total) {
-        this.href = href;
-        this.total = total;
-    }
 
     public Object getHref() {
         return href;
